8aecbe5740596475647dd19f91dcd5ba328afa84
KN250700 <nguyenthikimngan250700@gmail.com>
2021-06-15 22:03:25 +0700

    bfstsgf

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,12 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'salomon')
+$conn = mysqli_connect('localhost', 'root', '', 'salomon')
 or die('khong het nối được database.');
 
 
 // tùy chỉnh kết nối
 // set charset utf8 đối với kết nối này. dùng để gõ tiếng việt, nhật ,thái trung quốc
 // lưu ý: gõ với bộ unikey, bằng mã unicode
-$conn->query("SET NAME 'utf8'");
+$conn->query("SET NAMES 'utf8'");
 $conn->query("SETCHARACTER SET UTF8");
-$conn->query("SET SESSION collation_connection = 'utf8_general_ci");
+$conn->query("SET SESSION collation_connection = 'utf8_general_ci'");
 ?>
